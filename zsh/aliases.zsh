b9fbc5883188b3d7a3ba3913659c599814ce2a2c
Marco Adkins <10146618+madkin10@users.noreply.github.com>
2020-02-05 10:33:28 -0500

    Make complicated kboard alias a function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -26,6 +26,10 @@ alias start-pg='brew services start postgresql'
 alias stop-pg='brew services stop postgresql'
 alias gatling='/usr/local/lib/gatling-charts-highcharts-bundle-3.0.0-RC2/bin/gatling.sh'
 alias myip='ipconfig getifaddr en0'
-alias kboard="kubectl proxy & kubectl get secret --namespace kube-system $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}') -o jsonpath="{.data.token}" | base64 --decode | pbcopy  && sleep 2 && open 'http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#!/login' && fg"
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 alias dotfiles="cd ~/.dotfiles"
+
+function kboard(){
+  kubectl proxy & kubectl get secret --namespace kube-system $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}') -o jsonpath="{.data.token}" | base64 --decode | pbcopy  && sleep 2 && open 'http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#!/login' && fg
+}
+
